4c30da0142eb4654eb1ae053a043a6a43040e7d2
henke164 <henrik.mattsson@hotmail.com>
2017-11-16 17:06:49 +0100

    input now also handles mouseclicks

--- src/input.js
+++ src/input.js
@@ -1,11 +1,36 @@
 class Input {
     constructor(canvas) {
-        this.mousePosition = new Vector2(0, 0);
+        this.mouseState = {
+            position: new Vector2(0, 0),
+            leftButtonDown: false,
+            rightButtonDown: false,
+        };
 
-        var t = this;
-        canvas.addEventListener('mousemove', function(evt) {
+        this.bindMouseEvents(canvas);
+    }
+
+    bindMouseEvents(canvas) {
+        canvas.addEventListener('mousemove', (evt) => {
             var rect = canvas.getBoundingClientRect();
-            t.mousePosition = new Vector2(evt.clientX - rect.left, evt.clientY - rect.top);
-        }, false);
-    };
+            this.mouseState.position = new Vector2(evt.clientX - rect.left, evt.clientY - rect.top);
+        });
+
+        canvas.addEventListener('mousedown', (evt) => {
+            if (evt.which == 1) {
+                this.mouseState.leftButtonDown = true;
+            }
+            if (evt.which == 3) {
+                this.mouseState.rightButtonDown = true;
+            }
+        });
+
+        canvas.addEventListener('mouseup', (evt) => {
+            if (evt.which == 1) {
+                this.mouseState.leftButtonDown = false;
+            }
+            if (evt.which == 3) {
+                this.mouseState.rightButtonDown = false;
+            }
+        });
+    }
 }
